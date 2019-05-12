c2a0c29c46263fa49f014c56c0fe77139dbe4b64
Lucandroid70 <43816108+lucandroid70@users.noreply.github.com>
2019-05-12 11:50:31 +0200

    Update webmin_install.sh

--- webmin_install.sh
+++ webmin_install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 sudo apt update
+
+sudo apt install build-essential -y
+
+sudo apt install wget curl -y
+
+sudo apt install git -y
+
 sudo apt install software-properties-common apt-transport-https wget -y
 
 wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -

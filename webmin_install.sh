20d381343582414ba9b2897c31620820693a3fbe
Lucandroid70 <43816108+lucandroid70@users.noreply.github.com>
2019-05-12 11:40:29 +0200

    Create webmin_install.sh

---
+++ webmin_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+
+wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] http://download.webmin.com/download/repository sarge contrib"
+
+
+sudo apt install webmin
+
+
+sudo ufw allow 10000/tcp
